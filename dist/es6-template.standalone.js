dccec686aa5f327b67d98cf90d8cdc2362b1c4a5
Nikolai Bezruk <bezruk.kolya@isells.ru>
2016-03-17 17:32:18 +0200

    Update es6-template.standalone.js

--- dist/es6-template.standalone.js
+++ dist/es6-template.standalone.js
@@ -99,7 +99,7 @@ function templateFn (str) {
     if (prop && prop.indexOf('.') !== -1) {
       return utils.getValue(data, prop)
     }
-    return typeof data[prop] !== 'undefined' ? data[prop] : ''
+    return typeof data[prop] !== 'undefined' ? data[prop] : '${'+ prop +'}'
   })
 }
 
